34ef46790c7c414acab0ccebf902d2a44163b9b4
sanjanaoney <iamoney012@gmail.com>
2023-04-07 01:42:06 +0600

    Assignment 1

---
+++ Assignment 1/multiply.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ long long int A,B, multi;
+scanf("%lld %lld",&A,&B);
+multi=A*B;
+printf("%lld", multi);
+return 0;
+}
+
+
---
+++ Assignment 1/shopping.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int N;
+scanf("%d",&N);
+if(N>1000)
+{
+    printf("I will buy Punjabi\n");
+
+    if(N-1000>=500)
+    {
+       printf("I will buy new shoes\n"); 
+       printf("Alisa will buy new shoes");
+       
+}
+}
+    
+    else {
+printf("Bad luck!");
+}
+
+
+return 0;
+
+}
