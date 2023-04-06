449141309fe79f65f20e1db65897cd8fca46ca4c
sanjanaoney <iamoney012@gmail.com>
2023-04-06 22:58:45 +0600

    Practice Day 2

---
+++ Practice Day 2/divisible_by_5_or_not.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d ",i);
+if(i%5==0)
+{
+printf("Yes\n");
+}
+ else
+{
+printf("No\n");
+}
+
+}
+return 0;
+
+}
---
+++ Practice Day 2/variables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int A;
+long long int B;
+float C;
+char D;
+scanf("%d\n",&A);
+scanf("%lld\n",&B);
+scanf("%f\n",&C);
+scanf("%c\n",&D);
+printf("%d\n%lld\n%.2f\n%c\n",A,B,C,D);
+return 0;
+
+
+}
